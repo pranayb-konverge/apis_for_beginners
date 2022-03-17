705abc8b9345b583365712189a5b467add6b7e5f
Pranay Bankar <pranay.bankar@konverge.ai>
2022-03-17 16:15:09 +0530

    update he code

--- python API code/code/explore.py
+++ python API code/code/explore.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 
 client = Client("AC1ab13a200bd2868847b088655309c747", "6035694070272c018b9c03a57e37b9c5")
 
-# for message in client.messages.list():
-#     print(message.body)
+for message in client.messages.list():
+    print(message.body)
 
 message = client.messages.create(
     to="+917030026662",
